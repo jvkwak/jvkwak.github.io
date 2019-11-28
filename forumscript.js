f54af293b1fb5ca17d66f914d0464f4af5af89be
wonbeenkim_2001 <31658667+Wonbeen@users.noreply.github.com>
2019-11-28 17:38:08 +0900

    Update forumscript.js

--- forumscript.js
+++ forumscript.js
@@ -1,1 +1,7 @@
-
+var text = document.getElementById("demo").textContent;
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get("uname")
+const email = urlParams.get("email");
+const comment = urlParams.get("comment");
+text += "username: " + username + "<br>" + "email: " + email + "<br>" + "question: " + comment + "<br><br>";
+document.getElementById("demo").innerHTML = text;
